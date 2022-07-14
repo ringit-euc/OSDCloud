9d8a9f01eda8af9f34bde6a2e018d38522c83a20
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2022-07-15 00:49:43 +0800

    Update Test-OSDCloud_ZTI.ps1

--- Test-OSDCloud_ZTI.ps1
+++ Test-OSDCloud_ZTI.ps1
@@ -1,33 +1,30 @@
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
+Start-Sleep -Seconds 5
 
-#================================================
-#   [PreOS] Update Module
-#================================================
+#Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1366x"
+    Set-DisRes 1366
 }
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "1: Install Windows 10 21H2 | en-us | Professional"-ForegroundColor Yellow
+Write-Host "2: Exit" -ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force -SkipPublisherCheck
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
-Import-Module OSD -Force   
+Write-Host  -ForegroundColor Cyan "Importing PowerShell Modules"
+Import-Module OSD -Force
 
-#=======================================================================
-#   [OS] Params and Start-OSDCloud
-#=======================================================================
-$Params = @{
-    OSVersion = "Windows 10"
-    OSBuild = "21H2"
-    OSEdition = "Pro"
-    OSLanguage = "en-us"
-    OSLicense = "Retail"
-    ZTI = $false
-    Firmware = $true
-    SkipAutopilot = $true
-    SkipODT = $true
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild 21H2 -OSEdition Pro -OSLicense Retail -ZTI -Firmware -SkipAutopilot -SkipODT }
+    '2' { Exit }  
 }
-Start-OSDCloud @Params
 
 #================================================
 #  [PostOS] OOBEDeploy Configuration
