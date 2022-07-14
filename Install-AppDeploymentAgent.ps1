5e44679a06491758a1730f314db52739ed419118
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2022-07-15 07:07:28 +0800

    Update Install-AppDeploymentAgent.ps1

--- Install-AppDeploymentAgent.ps1
+++ Install-AppDeploymentAgent.ps1
@@ -2,8 +2,13 @@
 # https://kb.vmware.com/s/article/87801?lang=en_US&queryTerm=sfd%20agent
 
 # Download the appdeploymentagent-x64.msi from Google Drive
-$zipFile = "https://drive.google.com/uc?export=download&id=16XPdOK8K_mzu6XPGSuyKJVi2ODEL8old"
-Invoke-WebRequest -Uri $zipFile -OutFile "C:\Temp\appdeploymentagent-x64.msi"
+$SaveFolder = "C:\Temp"
+$InstallerFileName = "appdeploymentagent-x64.msi"
+$Url = "https://drive.google.com/uc?export=download&id=16XPdOK8K_mzu6XPGSuyKJVi2ODEL8old&confirm=t"
+
+$SavePath = Join-Path $SaveFolder $InstallerFileName
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($Url,$SavePath)
 
 # Install the appdeploymentagent-x64.msi silently
 msiexec /i "C:\Temp\appdeploymentagent-x64.msi" /qn /norestart
