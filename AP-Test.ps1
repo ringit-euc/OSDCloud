7174d6c2daf8111b340015cbf2295223dc66ffd4
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2024-02-13 20:30:41 -0700

    Update AP-Test.ps1

--- AP-Test.ps1
+++ AP-Test.ps1
@@ -1,3 +1,8 @@
+# Retrieve secrets from environment variables
+$TenantId = $env:EIDTENANT
+$AppId = $env:EIDAPPID
+$AppSecret = $env:EIDAPPSECRET
+
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force:$true
 Install-Script get-windowsautopilotinfo -Confirm:$false -Force:$true
 get-windowsautopilotinfo -Online -TenantId $TenantId -AppId $AppId -AppSecret $AppSecret
