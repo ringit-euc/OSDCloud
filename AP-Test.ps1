38be7e62e45dacf35229e11a3f622001c1e6f7cf
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2024-02-13 18:18:44 -0700

    Create AP-Test.ps1

---
+++ AP-Test.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Script -Name Get-WindowsAutoPilotInfo -Force | Out-Null
+Install-Module -Name WindowsAutopilotIntune -Force | Out-Null
+
+$AutopilotParams = @{
+    Online = $true
+    TenantId = "${{secrets.EIDTENANT}}"
+    AppId = "${{secrets.EIDAPPID}}"
+    AppSecret = "${{secrets.EIDAPPSECRET}}"
+    }
+    Get-WindowsAutoPilotInfo @AutopilotParams
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
