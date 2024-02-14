ba7d7487dbc1d9af55358d75cf460944e968bb4c
ringit-euc <108431092+ringit-euc@users.noreply.github.com>
2024-02-13 20:29:46 -0700

    Update AP-Test.ps1

--- AP-Test.ps1
+++ AP-Test.ps1
@@ -1,21 +1,3 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
-
-Install-PackageProvider -Name NuGet -Force | Out-Null
-Install-Script -Name Get-WindowsAutoPilotInfo -Force | Out-Null
-Install-Module -Name WindowsAutopilotIntune -Force | Out-Null
-
-# Retrieve secrets from environment variables
-$TenantId = $env:EIDTENANT
-$AppId = $env:EIDAPPID
-$AppSecret = $env:EIDAPPSECRET
-
-$AutopilotParams = @{
-    Online = $true
-    TenantId = $TenantId
-    AppId = $AppId
-    AppSecret = $AppSecret
-}
-
-Get-WindowsAutoPilotInfo @AutopilotParams
-
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force:$true
+Install-Script get-windowsautopilotinfo -Confirm:$false -Force:$true
+get-windowsautopilotinfo -Online -TenantId $TenantId -AppId $AppId -AppSecret $AppSecret
